ebb3674ced1a0a629f476ef3319548b2d3e536e1
Umang Vadadoriya <umang.vadadoriya@bbd.co.za>
2024-03-12 19:22:29 +0530

    Deploy Version 2

--- src/main/java/com/hybrid/hybridhavenapi/Controller/EmployeeController.java
+++ src/main/java/com/hybrid/hybridhavenapi/Controller/EmployeeController.java
@@ -18,7 +18,7 @@ public class EmployeeController {
 
     @GetMapping("/")
     public String test(){
-        return "Welcome To Hybrid Haven API";
+        return "Welcome To Hybrid Haven API Version 2";
     }
 
     @GetMapping("employees")
