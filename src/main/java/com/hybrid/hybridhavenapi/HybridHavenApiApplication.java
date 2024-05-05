a4b6a5fb6581a7e75b2555a737cdfbf9e10ca387
Umang Vadadoriya <160090850+Umang-Vadadoriya-BBD@users.noreply.github.com>
2024-05-05 10:52:22 +0530

    Restart The EC2
    Sorry

--- src/main/java/com/hybrid/hybridhavenapi/HybridHavenApiApplication.java
+++ src/main/java/com/hybrid/hybridhavenapi/HybridHavenApiApplication.java
@@ -9,7 +9,6 @@ public class HybridHavenApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HybridHavenApiApplication.class, args);
 		System.out.println("\n ==> Ready To Tackle The Requests...");
-
 	}
 
 }
