cf36cb01a6f46de144b8099003ed5d16c25ea395
krunal-BBD <krunal.rana@bbd.co.za>
2024-05-05 12:17:15 +0530

    HH-28 Adding 2 apis for vacation and office

--- src/main/java/com/hybrid/hybridhavenapi/Repository/VacationRepository.java
+++ src/main/java/com/hybrid/hybridhavenapi/Repository/VacationRepository.java
@@ -2,6 +2,12 @@ package com.hybrid.hybridhavenapi.Repository;
 
 import com.hybrid.hybridhavenapi.Entity.Vacation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface VacationRepository extends JpaRepository<Vacation,Integer> {
+    @Query("SELECT v FROM Vacation v WHERE :date BETWEEN v.vacationStartDate AND v.vacationEndDate")
+    public List<Vacation> findVacationsByDate(Date date);
 }
