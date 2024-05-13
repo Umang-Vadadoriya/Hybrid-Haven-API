cfb433c09533237e18be8d199d3f526d5269e817
Umang Vadadoriya <Umang.Vadadoriya@bbd.co.za>
2024-05-13 13:42:16 +0530

    Update webconfig.java

--- src/main/java/com/hybrid/hybridhavenapi/Config/webconfig.java
+++ src/main/java/com/hybrid/hybridhavenapi/Config/webconfig.java
@@ -9,7 +9,7 @@ public class webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://34.251.172.36:8080") // Allow requests from any origin
+                .allowedOrigins("http://127.0.0.1:5500", "http://34.251.172.36:8080", "http://34.251.172.36:5500") // Allow requests from any origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow only specific methods
                 .allowCredentials(true)
                 .allowedHeaders("*");
